8fae484a6600d36b49c8a422da96d41cb10dffdd
Mikhail Gozhenko <sacred9900@gmail.com>
2021-10-24 00:02:13 +0500

    test failed

--- src/test/java/ru/netology/rest/MobileBankApiTestV4.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV4.java
@@ -5,20 +5,19 @@ import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
-class MobileBankApiTestV4 {
-    @Test
+class MobileBankApiTest {
+    @Test // этот тест падает при проверке Currency
     void shouldReturnDemoAccounts() {
-      // Given - When - Then
-      // Предусловия
-      given()
-          .baseUri("http://localhost:9999/api/v1")
-      // Выполняемые действия
-      .when()
-          .get("/demo/accounts")
-      // Проверки
-      .then()
-          .statusCode(200)
-          .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
-      ;
+
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+
+                .when()
+                .get("/demo/accounts")
+
+                .then()
+                .statusCode(200)
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+        ;
     }
 }
